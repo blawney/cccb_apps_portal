dd14ac819fb0684b640535f63fa6192e2d93553a
Brian <brian_lawney@mail.dfci.harvard.edu>
2017-06-07 16:37:28 +0000

    Disable the analyze button so they cannot click it twice

--- static/js/summary.js
+++ static/js/summary.js
@@ -2,6 +2,7 @@ var analyzeButton = document.getElementById("start-analysis");
 var pkField = document.getElementById("pk-field");
 
 analyzeButton.addEventListener("click", function(e){
+	e.target.disabled = true;
 	var pk = pkField.value;
 	window.location.assign("/analysis/do-analysis/" + pk + "/")
 });
